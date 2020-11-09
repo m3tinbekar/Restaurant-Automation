505eea176bcfc89ae48473115780c14447f804ea
m3tinbekar <57272431+m3tinbekar@users.noreply.github.com>
2020-11-09 15:49:21 +0300

    Restaurant-Automation

---
+++ Restaurant-Automation/RestaurantOtomasyon/RestaurantOtomasyon/Genel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOtomasyon
+{
+    class Genel
+    {
+
+        public string conString = (@"Data Source = BEKAROGLU\BEKAROGLU; Initial Catalog = nesneRestoranOtomasyon; Integrated Security = True");
+        public int _personelId;
+        public int _gorevId;
+        
+
+
+
+
+    }
+}
